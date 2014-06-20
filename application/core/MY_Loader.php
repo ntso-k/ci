6038b397ed26fcc18004846c43ba189897f8df07
ntso-k <ntso.king@gmail.com>
2014-06-20 23:42:24 +0800

    add MY_Loader.php to support themes of view

---
+++ application/core/MY_Loader.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zide
+ * Date: 14-4-12
+ * Time: 下午3:04
+ */
+class MY_Loader extends CI_Loader {
+	const VIEW_FILE_EXT = '.view.php';
+
+	public function __construct()
+	{
+		parent::__construct();
+		
+		$this->_ci_view_paths = array(APPPATH.'views/base/'	=> TRUE);
+	}
+}
